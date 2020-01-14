8a91d27e40d2448d583ddbb60cb8888eb7bd2dc5
Tom Lowry <38481365+thomas-lowry@users.noreply.github.com>
2020-01-14 11:21:04 -0500

    Added import/export for isInstanceNode helper

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 //import all helper functions here
 import getPage from './helpers/getPage.js';
+import isInstanceNode from './helpers/isInstanceNode.js'
 import topLevelFrames from './helpers/topLevelFrames.js';
 import removeDuplicatesByKeyValue from './helpers/removeDuplicatesByKeyValue.js'
 
 //export all helper functions so they can be used indidually as needed
-export { getPage, topLevelFrames, removeDuplicatesByKeyValue };
+export { getPage, isInstanceNode, topLevelFrames, removeDuplicatesByKeyValue };
