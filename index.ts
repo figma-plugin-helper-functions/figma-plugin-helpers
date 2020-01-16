a2ffbbe9410cc0147df1e295d06c4c6c70203d35
Thomas Lowry <tom@negativespace.ca>
2020-01-16 00:36:19 -0500

    moving scripts into src dir

--- index.ts
+++
@@ -1,8 +0,0 @@
-//import all helper functions here
-import getPage from './helpers/getPage';
-import getIndexNode from './helpers/getIndexNode';
-import isInstanceNode from './helpers/isInstanceNode'
-import topLevelFrames from './helpers/topLevelFrames';
-
-//export all helper functions so they can be used indidually as needed
-export { getPage, getIndexNode, isInstanceNode, topLevelFrames };
