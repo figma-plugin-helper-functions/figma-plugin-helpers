6baa077fb727d9a369891e991c6331d48c6a1d18
alexandrtovmach <alexandrtovmach@gmail.com>
2020-02-21 16:30:47 +0200

    feat: Strictified arguments

--- src/helpers/isTypeNode.ts
+++ src/helpers/isTypeNode.ts
@@ -1,27 +1,27 @@
-export const isPageNode = (node: any): node is PageNode => {
+export const isPageNode = (node: BaseNode): node is PageNode => {
   return node.type === "PAGE";
 };
 
-export const isGroupNode = (node: any): node is GroupNode => {
+export const isGroupNode = (node: BaseNode): node is GroupNode => {
   return node.type === "GROUP";
 };
 
-export const isFrameNode = (node: any): node is FrameNode => {
+export const isFrameNode = (node: BaseNode): node is FrameNode => {
   return node.type === "FRAME";
 };
 
-export const isTextNode = (node: any): node is TextNode => {
+export const isTextNode = (node: BaseNode): node is TextNode => {
   return node.type === "TEXT";
 };
 
-export const isInstanceNode = (node: any): node is InstanceNode => {
+export const isInstanceNode = (node: BaseNode): node is InstanceNode => {
   return node.type === "INSTANCE";
 };
 
-export const isComponentNode = (node: any): node is ComponentNode => {
+export const isComponentNode = (node: BaseNode): node is ComponentNode => {
   return node.type === "COMPONENT";
 };
 
-export const isOneOfNodeType = (node: any, typeList: string[]) => {
+export const isOneOfNodeType = (node: BaseNode, typeList: NodeType[]) => {
   return typeList.includes(node.type);
 };
