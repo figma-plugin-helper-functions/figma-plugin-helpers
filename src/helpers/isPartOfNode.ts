b6879f9a412b2f8a3e170566af63b95facfd557f
Jonas Sicking <jonas@sicking.cc>
2020-03-06 10:17:26 -0800

    Avoid hanging for nodes in deleted/remote symbols

--- src/helpers/isPartOfNode.ts
+++ src/helpers/isPartOfNode.ts
@@ -1,11 +1,10 @@
 //this function allows you to check whether a node is part of an rootNode
-export default function isPartOfNode(part: SceneNode, rootNode: BaseNode): boolean {
-	const parent = part.parent
-	if (parent === rootNode) {
-		return true
-	} else if (parent.type === 'PAGE') {
-		return false
-	} else {
-		return isPartOfNode(parent as SceneNode, rootNode)
+export default function isPartOfNode(part: BaseNode?, rootNode: BaseNode): boolean {
+	while (part) {
+		if (part === rootNode) {
+			return true
+		}
+		part = part.parent
 	}
+	return false
 }
