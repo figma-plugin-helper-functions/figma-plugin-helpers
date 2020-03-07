2d9732538cfc8fc6a67b5cdda0504ac2ab52822b
Andreslav <andreslav@github.com>
2020-03-07 22:27:22 +0300

    Update topLevelFrames.ts

--- src/helpers/topLevelFrames.ts
+++ src/helpers/topLevelFrames.ts
@@ -1,4 +1,4 @@
 //this function returns all top level frames on currentPage
-export default function topLevelFrames() {
-	return figma.currentPage.children.filter((node) => node.type === 'FRAME') as FrameNode[]
+export default function topLevelFrames(page: PageNode = figma.currentPage) {
+	return page.children.filter((node) => node.type === 'FRAME') as FrameNode[]
 }
