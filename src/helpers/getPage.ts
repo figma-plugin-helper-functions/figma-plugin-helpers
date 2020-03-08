678261fd1bcb5253da6d5f67a9a8750a1022a7ba
Andreslav <andreslav@github.com>
2020-03-08 21:29:37 +0300

    Update getPage.ts
    Fix type errors

--- src/helpers/getPage.ts
+++ src/helpers/getPage.ts
@@ -1,7 +1,7 @@
 //this function allows you to pass in a node and return its pageNode
-export default function getPage(node: BaseNode): PageNode {
+export default function getPage(node: BaseNode) {
 	while (node && node.type != 'PAGE') {
 		node = node.parent
 	}
-	return node
+	return node as PageNode
 }
