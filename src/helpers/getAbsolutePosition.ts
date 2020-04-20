d4069e15734374b3dcddb9ae54d4143dd97a5638
alexandrtovmach <alexandrtovmach@gmail.com>
2020-04-20 19:51:38 +0300

    feat: Added getAbsPos helper

---
+++ src/helpers/getAbsolutePosition.ts
@@ -0,0 +1,17 @@
+import { isInstanceNode, isGroupNode, isPageNode } from '../'
+
+// calculate absolute position of node on page by asix
+const getAbsolutePosition = (node: BaseNode, asix: 'x' | 'y') => {
+	if (!node) {
+		return 0
+	} else if (isInstanceNode(node) && isGroupNode(node.parent)) {
+		return node[asix]
+	} else if (isGroupNode(node)) {
+		return getAbsolutePosition(node.parent, asix)
+	} else if (!isPageNode(node.parent)) {
+		return node[asix] + getAbsolutePosition(node.parent, asix)
+	}
+	return 0
+}
+
+export default getAbsolutePosition
