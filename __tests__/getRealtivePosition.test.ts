00686fe7ea40df5503534057e2a1c1680d49b1f2
alexandrtovmach <alexandrtovmach@gmail.com>
2020-04-21 22:55:57 +0300

    feat: Added condition

--- __tests__/getRealtivePosition.test.ts
+++ __tests__/getRealtivePosition.test.ts
@@ -1,66 +1,80 @@
 import { createFigma } from 'figma-api-stub'
-import { getRealtivePosition } from '../src'
+import { getRealtivePosition, isFrameNode, getNodeIndex } from '../src'
 
 const GAP = 50
 const figma = createFigma({})
 const pageNode = figma.createPage()
 const frameNode1 = figma.createFrame()
 const frameNode2 = figma.createFrame()
+const frameNode3 = figma.createFrame()
+const frameNode4 = figma.createFrame()
 const rectangleNode = figma.createRectangle()
 const textNode = figma.createText()
 const groupNode = figma.group([rectangleNode, textNode], pageNode)
-frameNode2.appendChild(groupNode)
-frameNode1.appendChild(frameNode2)
+frameNode4.appendChild(groupNode)
+const frames = [frameNode1, frameNode2, frameNode3, frameNode4]
+frames.forEach((n, i, arr) => {
+	n.x = 0
+	n.y = 0
+	n.name = `frameNode${i + 1}`
+	arr[Math.max(i - 1, 0)].appendChild(n)
+})
 pageNode.appendChild(frameNode1)
 
 describe('getRealtivePosition', () => {
-    frameNode1.x = 0
-    frameNode1.y = 0
-    frameNode2.x = 0
-    frameNode2.y = 0
-    groupNode.x = 0
-    groupNode.y = 0
-    rectangleNode.x = 0
-    rectangleNode.y = 0
-    textNode.x = 0
-    textNode.y = 0
+	groupNode.x = 0
+	groupNode.y = 0
+	rectangleNode.x = 0
+	rectangleNode.y = 0
+	textNode.x = 0
+	textNode.y = 0
 	test('"x" asix calculations for zero values', () => {
 		expect(getRealtivePosition(textNode, 'x')).toBe(0)
 	})
 	test('"x" asix calculations for custom values', () => {
-        frameNode2.x = GAP
-        textNode.x = GAP
-		expect(getRealtivePosition(textNode, 'x')).toBe(GAP*2)
+		frameNode2.x = GAP
+		textNode.x = GAP
+		expect(getRealtivePosition(textNode, 'x')).toBe(GAP * 2)
 	})
 	test('"x" asix calculations should ignore GroupNode position', () => {
-        frameNode2.x = GAP
-        groupNode.x = GAP
-        textNode.x = GAP
-		expect(getRealtivePosition(textNode, 'x')).toBe(GAP*2)
+		frameNode2.x = GAP
+		groupNode.x = GAP
+		textNode.x = GAP
+		expect(getRealtivePosition(textNode, 'x')).toBe(GAP * 2)
 	})
 	test('"x" asix calculations should ignore top level node position', () => {
-        frameNode1.x = GAP
-        frameNode2.x = GAP
-        textNode.x = GAP
-		expect(getRealtivePosition(textNode, 'x')).toBe(GAP*2)
+		frameNode1.x = GAP
+		frameNode2.x = GAP
+		textNode.x = GAP
+		expect(getRealtivePosition(textNode, 'x')).toBe(GAP * 2)
+	})
+	test('"x" asix calculations with condition', () => {
+		frameNode2.x = GAP
+		textNode.x = GAP 
+		expect(getRealtivePosition(textNode, 'x', (node) => node.name === 'frameNode3')).toBe(GAP)
 	})
 	test('"y" asix calculations for zero values', () => {
 		expect(getRealtivePosition(textNode, 'y')).toBe(0)
 	})
 	test('"y" asix calculations for zero values', () => {
-        frameNode2.y = GAP
-        textNode.y = GAP
-		expect(getRealtivePosition(textNode, 'y')).toBe(GAP*2)
+		frameNode2.y = GAP
+		textNode.y = GAP
+		expect(getRealtivePosition(textNode, 'y')).toBe(GAP * 2)
 	})
 	test('"y" asix calculations should ignore GroupNode position', () => {
-        frameNode2.y = GAP
-        textNode.y = GAP
-		expect(getRealtivePosition(textNode, 'y')).toBe(GAP*2)
+		frameNode2.y = GAP
+		textNode.y = GAP
+		expect(getRealtivePosition(textNode, 'y')).toBe(GAP * 2)
 	})
 	test('"y" asix calculations should ignore top level node position', () => {
-        frameNode1.y = GAP
-        frameNode2.y = GAP
-        textNode.y = GAP
-		expect(getRealtivePosition(textNode, 'y')).toBe(GAP*2)
+		frameNode1.y = GAP
+		frameNode2.y = GAP
+		textNode.y = GAP
+		expect(getRealtivePosition(textNode, 'y')).toBe(GAP * 2)
+	})
+	test('"y" asix calculations with condition', () => {
+		frameNode2.y = GAP
+		textNode.y = GAP 
+		expect(getRealtivePosition(textNode, 'y', (node) => node.name === 'frameNode3')).toBe(GAP)
 	})
 })
--- src/helpers/getRealtivePosition.ts
+++ src/helpers/getRealtivePosition.ts
@@ -1,17 +1,23 @@
 import { isInstanceNode, isGroupNode, isPageNode } from '..'
 
-// calculate absolute position of node by asix based on top level element
-const getRealtivePosition = (node: BaseNode, asix: 'x' | 'y') => {
-	if (!node) {
+/**
+ *  calculate relative position of node by asix
+ **/
+const getRealtivePosition = (
+	node: BaseNode,
+	asix: 'x' | 'y',
+	conditionFunc?: (node: BaseNode) => boolean
+) => {
+	conditionFunc = conditionFunc || (() => false)
+	if (!node || conditionFunc(node.parent) || isPageNode(node.parent)) {
 		return 0
 	} else if (isInstanceNode(node) && isGroupNode(node.parent)) {
 		return node[asix]
 	} else if (isGroupNode(node)) {
-		return getRealtivePosition(node.parent, asix)
-	} else if (!isPageNode(node.parent)) {
-		return node[asix] + getRealtivePosition(node.parent, asix)
+		return getRealtivePosition(node.parent, asix, conditionFunc)
+	} else {
+		return node[asix] + getRealtivePosition(node.parent, asix, conditionFunc)
 	}
-	return 0
 }
 
 export default getRealtivePosition
