335e30673365e6eaf3a879d20161af7bf5b73148
alexandrtovmach <alexandrtovmach@gmail.com>
2020-05-09 12:10:18 +0300

    feat: Updated findAll helper; Covered with test

--- src/helpers/findMethods.ts
+++ src/helpers/findMethods.ts
@@ -1,7 +1,16 @@
 import { hasChildren } from '..'
 
 /**
- * Custom implementation of the figma.findAll method, which runs x1000 times faster
+ * Custom implementation of the figma.findAll method, which runs x1000 times faster.
+ * 
+ * ### Usage example
+ * ```ts
+ * import { findAll, isTextNode } from "@figma-plugin/helpers"
+ * 
+ * const textNodes = findAll(figma.currentPage.children, isTextNode)
+ * ```
+ * 
+ * ### How to replace native `figma.findAll`
  * ```diff
  * + import { findAll } from "@figma-plugin/helpers"
  * 
@@ -11,13 +20,13 @@ import { hasChildren } from '..'
  */
 export const findAll = (
 	nodes: readonly SceneNode[],
-	iteratee?: (elem?: BaseNode, idx?: number, array?: readonly SceneNode[]) => boolean
+	iteratee: (elem?: BaseNode, idx?: number, array?: readonly SceneNode[]) => boolean
 ) => {
 	return nodes.reduce((prev, el, i, arr) => {
 		if (iteratee(el, i, arr)) {
 			prev.push(el)
 		} else if (hasChildren(el)) {
-			prev.push(...findAll(el.children))
+			prev.push(...findAll(el.children, iteratee))
 		}
 		return prev
 	}, [])
