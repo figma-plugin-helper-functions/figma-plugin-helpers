c336fcd18af82c8a5613f4784962ec1981660391
alexandrtovmach <alexandrtovmach@gmail.com>
2020-05-09 12:23:13 +0300

    refactor: Prettified

--- src/helpers/findMethods.ts
+++ src/helpers/findMethods.ts
@@ -2,18 +2,18 @@ import { hasChildren } from '..'
 
 /**
  * Custom implementation of the figma.findAll method, which runs x1000 times faster.
- * 
+ *
  * ### Usage example
  * ```ts
  * import { findAll, isTextNode } from "@figma-plugin/helpers"
- * 
+ *
  * const textNodes = findAll(figma.currentPage.children, isTextNode)
  * ```
- * 
+ *
  * ### How to replace native `figma.findAll`
  * ```diff
  * + import { findAll } from "@figma-plugin/helpers"
- * 
+ *
  * - const textNodes = figma.currentPage.findAll((node) => node.type === "TEXT");
  * + const textNodes = findAll(figma.currentPage.children, (node) => node.type === "TEXT")
  * ```
--- src/helpers/hasChildren.ts
+++ src/helpers/hasChildren.ts
@@ -1,16 +1,16 @@
 /**
  * Checks node to have children nodes
  * For example:
- * 
+ *
  * ```ts
  * // BEFORE
  * console.log(node.children) // throw TS error "Property 'children' does not exist on type ..."
- * 
+ *
  * // AFTER
  * if (hasChildren(node)) {
  *  console.log(node.children) // valid code
  * }
  * ```
- * 
+ *
  */
 export default (node: BaseNode): node is BaseNode & ChildrenMixin => Boolean(node['children'])
