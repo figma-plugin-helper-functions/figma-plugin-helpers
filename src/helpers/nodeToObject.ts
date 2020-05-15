24270cfc4fd34b20cf8236642ea4ca4ebb233df0
alexandrtovmach <alexandrtovmach@gmail.com>
2020-05-15 15:36:47 +0300

    fix: Updated nodeToObject helper to handle failed get; Added flag;

--- src/helpers/nodeToObject.ts
+++ src/helpers/nodeToObject.ts
@@ -1,15 +1,27 @@
-export default function nodeToObject(node) {
+const nodeToObject = (node: any, withoutRelations?: boolean) => {
 	const props = Object.entries(Object.getOwnPropertyDescriptors(node.__proto__))
 	const blacklist = ['parent', 'children', 'removed', 'masterComponent']
 	const obj: any = { id: node.id, type: node.type }
-	if (node.parent) obj.parent = { id: node.parent.id, type: node.parent.type }
 	for (const [name, prop] of props) {
-		if (prop.get && blacklist.indexOf(name) < 0) {
-			obj[name] = prop.get.call(node)
-			if (typeof obj[name] === 'symbol') obj[name] = 'Mixed'
+		if (prop.get && blacklist.includes(name)) {
+			try {
+				obj[name] = prop.get.call(node)
+				if (typeof obj[name] === 'symbol') obj[name] = 'Mixed'
+			} catch (err) {
+				obj[name] = undefined
+			}
 		}
 	}
-	if (node.children) obj.children = node.children.map((child) => nodeToObject(child))
-	if (node.masterComponent) obj.masterComponent = nodeToObject(node.masterComponent)
+	if (node.parent && !withoutRelations) {
+		obj.parent = { id: node.parent.id, type: node.parent.type }
+	}
+	if (node.children && !withoutRelations) {
+		obj.children = node.children.map((child: any) => nodeToObject(child, withoutRelations))
+	}
+	if (node.masterComponent && !withoutRelations) {
+		obj.masterComponent = nodeToObject(node.masterComponent, withoutRelations)
+	}
 	return obj
 }
+
+export default nodeToObject
