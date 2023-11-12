ddcfdcf6629e29e7d5b03ea995863d984d7cca2f
Liam Martens <hi@liammartens.com>
2023-11-12 10:49:50 -0500

    fix: y bug

--- src/helpers/extractImageCropParams.ts
+++ src/helpers/extractImageCropParams.ts
@@ -18,7 +18,7 @@ export function extractImageCropParams(shapeWidth: number, shapeHeight: number, 
 		[0, 1]
 	].map((p) => {
 		const [x, y] = applyMatrixToPoint(mxInv, p)
-		return [x * shapeWidth, y * shapeWidth]
+		return [x * shapeWidth, y * shapeHeight]
 	})
 	const angle = Math.atan2(points[1][1] - points[0][1], points[1][0] - points[0][0])
 	const sizex = Math.sqrt(
